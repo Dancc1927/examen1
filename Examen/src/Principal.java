5267fa86d33724e27def6094b897c894d67f8a82
unknown <SenaManana@ARMAPRCCIFOD656.FormacionSena.RED>
2024-06-26 11:16:00 -0500

    uno

---
+++ Examen/src/Principal.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
---
+++ Examen/src/Procesos.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+	
+	
+public class Procesos {
+	public static void main(String[] args) {
+		
+	
+   ArrayList cantidad= new ArrayList();
+       cantidad.add(1);
+       cantidad.add(10.001);
+       cantidad.add(20.001);
+       cantidad.add(30.001);
+   ArrayList precios= new ArrayList();
+       precios.add(10.000);
+       precios.add(20.000);
+       precios.add(30.000);
+   
+   
+	   for(int n=0; n< cantidadP; n++) {
+		   System.out.println("ingrese el nombre del producto"+(n++));
+		   if(cantidad.contains(1)) {
+		       pago=*0;
+			   System.out.println("Su descuento es"+descuento);
+		   }
+	   }
+   }
+   
+}
